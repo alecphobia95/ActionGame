187221b7cf2f1f2cabceaf458a4ceb3802aa4f2e
alecphobia95 <46802114+alecphobia95@users.noreply.github.com>
2022-05-22 20:36:20 -0400

    Prep work
    Organizing and preparing some stuff to turn this 2.5d and organize new scene elements

---
+++ Action Prototype/Assets/Scripts/PlayerStateScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerStateScript : MonoBehaviour
+{
+    public string[] StaggerOptionArray = new string[] {"Staggerable", "Unstaggerable" };
+    public string[] HitOptionArray = new string[] {"NeutralHitState", "HitstunState", "RecoveryHitState" };
+
+    public string StaggerState = "Staggerable";
+    public string HitState = "NeutralHitState";
+    public GameObject HitBox;
+
+    public float HitStunTime;
+    public float StaggerTime;
+
+    private void HaveIBeenHit(Collision collision)
+    {
+        if(collision.gameObject.name == "Weapon")
+        {
+
+        }
+    }
+
+    private string WeaponState(Collision collision)
+    {
+        string state = "neutral";
+        GameObject weapon = collision.gameObject;
+        WeaponStateClass weaponState;
+
+        return (state);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+}
---
+++ Action Prototype/Assets/Scripts/WeaponStateClass.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponStateClass
+{
+    public string[] WeaponState = new string[] {"Neutral", "Active", "HitPause" };
+    public string CurrentState = "Neutral";
+
+}
